70d836c5faf2e158e1bc4268e785cb8ade253db1
psd <psd@whatfettle.com>
2015-12-03 07:00:11 +0000

    Refactored address matching

--- Makefile
+++ Makefile
@@ -2,13 +2,14 @@ DATE:=$(shell date +"%Y%m%d")
 EDUBASE_URL=http://www.education.gov.uk/edubase/edubasealldata$(DATE).csv
 
 REGISTERS=\
-	data/school/school.tsv
+	data/school/schools.tsv
 
 all:: flake8 $(REGISTERS)
 
-data/school/school.tsv: bin/schools.py cache/edubase.csv
+data/school/schools.tsv: bin/schools.py cache/edubase.csv
 	@mkdir -p data/school
 	bin/schools.py < cache/edubase.csv > $@
+
 
 # download from EDUBASE
 # - contains invalid UTF-8 characters ..
@@ -16,18 +17,16 @@ cache/edubase.csv:
 	@mkdir -p cache
 	curl -s $(EDUBASE_URL) | iconv -f ISO-8859-1 -t UTF-8 > $@
 
-clean::
-	rm -rf cache
-	rm -rf data
 
-#
-#  code
-#
 init::
 	pip3 install -r requirements.txt
 
 flake8:
 	flake8 bin
+
+prune::
+	rm -rf cache
+	rm -rf data
 
 clean::
 	-find . -name "*.pyc" | xargs rm -f
--- bin/schools.py
+++ bin/schools.py
@@ -1,70 +1,33 @@
 #!/usr/bin/env python3
 
 import sys
-import os
 import csv
-
 from datetime import datetime
 
-from pymongo import MongoClient
-
+from locate import address_match
 from gbgeo import osgb_to_wgs84
+
 from openregister import Item
 from openregister.representations.tsv import Writer
 
-mongo_uri = os.getenv('MONGO_URI', 'mongodb://127.0.0.1:27017/locate')
-
-fieldnames = ["school", "name", "start-date", "end-date", "gender",
-              "religious-character", "minimum-age", "maximum-age",
-              "headteacher", "website", "address"]
+fieldnames = [
+    "school",
+    "name",
+    "start-date", "end-date",
+    "gender", "religious-character",
+    "minimum-age", "maximum-age",
+    "headteacher",
+    "website",
+    "address"]
 
 writer = Writer(sys.stdout, fieldnames=fieldnames)
-uprns = set([])
 
 
 def http_url(url):
-    "Force HTTP URL"
     return url if url.startswith("http") else "http://" + url
 
 
-def close_enough(value, target):
-    return abs(value - target) < 0.001
-
-
-def get_best_match(row, addresses):
-    easting = row['Easting']
-    northing = row['Northing']
-    if (easting and northing):
-        longitude, latitude = osgb_to_wgs84(easting, northing)
-
-    for address in addresses:
-        try:
-            if not address['details']['classification'].startswith('CE0'):
-                continue
-
-            if latitude and longitude:
-                locate_lat = address['location']['lat']
-                locate_long = address['location']['long']
-                lat_close = close_enough(latitude, locate_lat)
-                long_close = close_enough(longitude, locate_long)
-                if lat_close and long_close:
-                    return address
-        except Exception as e:
-            print('Problem extracting address details', e)
-    else:
-        return {}
-
-
-def get_address_match(row, locate):
-    postcode = row['Postcode'].replace(' ', '').lower()
-    address_records = locate.find({"postcode": postcode})
-    return get_best_match(row, address_records)
-
-
 if __name__ == '__main__':
-
-    client = MongoClient(mongo_uri)
-    locate = client.get_default_database().addresses
 
     reader = csv.DictReader(sys.stdin)
 
@@ -90,8 +53,13 @@ if __name__ == '__main__':
         if row["SchoolWebsite"]:
             item.website = http_url(row["SchoolWebsite"])
 
-        address = get_address_match(row, locate)
-        if address:
-            item.address = address['uprn']
-            item.postcode = address['presentation']['postcode']
-            writer.write(item)
+        postcode = row['Postcode']
+
+        if row['Easting'] and row['Northing']:
+            lon, lat = osgb_to_wgs84(row['Easting'], row['Northing'])
+
+            address = address_match(postcode, lon, lat)
+            if address:
+                item.address = address['uprn']
+
+        writer.write(item)
