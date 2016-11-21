37384db54fde416b4fc41f4a9265059053a6eb57
robmckinnon <rob@movingflow>
2016-11-21 16:25:39 +0000

    Correct prefix on school organisation curie generation

--- bin/school-organisation.rb
+++ bin/school-organisation.rb
@@ -19,9 +19,9 @@ academy_types = type_codes('./lists/academy-types/types.txt', types)
 schools.each do |school|
   type = school.school_type
   school.organisation = if la_types.include? type
-                          "la-maintained-school:#{school.school_eng}"
+                          "la-maintained-school-eng:#{school.school_eng}"
                         elsif academy_types.include? type
-                          "academy-school:#{school.school_eng}"
+                          "academy-school-eng:#{school.school_eng}"
                         else
                           "school-type:#{type}"
                         end
